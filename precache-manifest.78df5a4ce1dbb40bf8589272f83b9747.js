2117d4788faba43dfd20593625957e56b979f933
Scott Elwood <scott.elwood@protonmail.com>
2020-04-25 11:59:06 -0400

    Updates

---
+++ precache-manifest.78df5a4ce1dbb40bf8589272f83b9747.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f5150044a092e8707e275e3443ad2a",
+    "url": "/scott-elwood/Robotfriends/index.html"
+  },
+  {
+    "revision": "d9a86e2ff48a8ab8f038",
+    "url": "/scott-elwood/Robotfriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "156f247704ea0f453c5c",
+    "url": "/scott-elwood/Robotfriends/static/css/main.c4244fdb.chunk.css"
+  },
+  {
+    "revision": "d9a86e2ff48a8ab8f038",
+    "url": "/scott-elwood/Robotfriends/static/js/2.1d024b66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scott-elwood/Robotfriends/static/js/2.1d024b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156f247704ea0f453c5c",
+    "url": "/scott-elwood/Robotfriends/static/js/main.018eac01.chunk.js"
+  },
+  {
+    "revision": "167cb084e5911d910276",
+    "url": "/scott-elwood/Robotfriends/static/js/runtime-main.2bfc9539.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/scott-elwood/Robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
