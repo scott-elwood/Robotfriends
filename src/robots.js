c8279858c96275e48c89ae75aafa5ffe982c3003
Scott Elwood <scott.elwood@protonmail.com>
2020-04-25 11:30:18 -0400

    first commit

---
+++ src/robots.js
@@ -0,0 +1,78 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Elliott Elwood',
+	username: 'LL',
+	email: 'ElEl@gmail.com',
+},
+{
+	id: 2,
+	name: 'Victoria Elwood',
+	username: 'Victori',
+	email: 'victori@gmail.com',
+},
+{
+	id: 3,
+	name: 'Momo Elwood',
+	username: 'Foofi',
+	email: 'foof@gmail.com',
+},
+{
+	id: 4,
+	name: 'Scott Elwood',
+	username: 'Hoobajoop',
+	email: 'scooter@gmail.com',
+},
+{
+	id: 5,
+	name: 'Paul Bargerstock',
+	username: 'Playtoh',
+	email: 'pbargs@gmail.com',
+},
+{
+	id: 6,
+	name: 'Joe Bargerstock',
+	username: 'eyecandy',
+	email: 'eyecandy@gmail.com',
+},
+{
+	id: 7,
+	name: 'Brian Elwood',
+	username: 'Giraffelove',
+	email: 'giraffelove@gmail.com',
+},
+{
+	id: 8,
+	name: 'Stefan Wyckoff',
+	username: 'palarity',
+	email: 'palarity@gmail.com',
+},
+{
+	id: 9,
+	name: 'Penelope',
+	username: 'Wolf',
+	email: 'preparedawolf@gmail.com',
+},
+{
+	id: 10,
+	name: 'Sebastian',
+	username: 'Jerk',
+	email: 'gom@gmail.com',
+},
+{
+	id: 11,
+	name: 'Ryan O\'Connor',
+	username: 'mandable',
+	email: '00connor@gmail.com',
+},
+{
+	id: 12,
+	name: 'Ryan O\'Brien',
+	username: 'coworker',
+	email: 'coworker@gmail.com',
+},
+
+
+]
+
+export default robots;
